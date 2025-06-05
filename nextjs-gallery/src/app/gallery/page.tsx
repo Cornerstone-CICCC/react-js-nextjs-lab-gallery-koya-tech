f3a6b59d774de0e3abe04fbf01c934404e1b01dd
koya <koya9431ptr@gmail.com>
2025-06-05 09:23:42 -0700

    setup

---
+++ nextjs-gallery/src/app/gallery/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function GalleryPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold mb-8">Gallery</h1>
+            {/* Gallery content will go here */}
+        </main>
+    );
+}
